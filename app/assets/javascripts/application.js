98bb4009326a37da5a6ac748aadac13e2d963601
Eric Mascot <slek120@gmail.com>
2015-08-30 17:38:07 -0500

    Bootstrap init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover'
+  });
+});
