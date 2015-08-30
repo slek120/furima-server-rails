87678e95443f1edc101befa429ff92aa51be91d1
Eric Mascot <slek120@gmail.com>
2015-08-30 12:13:57 -0500

    Remove omniauth gem

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secret.facebook_app_id, Rails.application.secret.facebook_app_secret
-end
